e0b05cc1eb9d17180ed80e1bfe98e5103072c128
dvvvvvv <dvvvvvv@dvvvvvv.com>
2019-12-31 14:37:35 +0900

    polish serializing tests

--- src/signal/serialize.rs
+++ src/signal/serialize.rs
@@ -50,7 +50,10 @@ fn test_serealizing_offer_signal() {
 
     let offer_signal_text = r#"{"type":"offer","name":"3872379c-4743-4a7d-b2ee-79cf7368cf58","target":"4fe681ad-aba1-4732-89df-ee784b7d4abf","sdp":"sdp"}"#;
 
-    assert_eq!(&serde_json::to_string(&offer_signal_struct).unwrap(), offer_signal_text);
+    assert_eq!(
+        &serde_json::to_string(&offer_signal_struct).unwrap(),
+        offer_signal_text
+    );
 }
 
 #[test]
@@ -65,21 +68,27 @@ fn test_serializing_answer_signal() {
 
     let answer_signal_text = r#"{"type":"answer","name":"3872379c-4743-4a7d-b2ee-79cf7368cf58","target":"4fe681ad-aba1-4732-89df-ee784b7d4abf","sdp":"sdp"}"#;
 
-    assert_eq!(&serde_json::to_string(&answer_signal_struct).unwrap(), answer_signal_text);
+    assert_eq!(
+        &serde_json::to_string(&answer_signal_struct).unwrap(),
+        answer_signal_text
+    );
 }
 
 #[test]
 fn test_serializing_new_ice_candidate_signal() {
     use super::IceCandidate;
 
-    let new_ice_candidate_struct = Signal::NewIceCandidate(IceCandidate{
+    let new_ice_candidate_struct = Signal::NewIceCandidate(IceCandidate {
         target: "4fe681ad-aba1-4732-89df-ee784b7d4abf".to_owned(),
         candidate: "candidate".to_owned(),
     });
 
     let ice_candidate_text = r#"{"type":"new_ice_candidate","target":"4fe681ad-aba1-4732-89df-ee784b7d4abf","candidate":"candidate"}"#;
 
-    assert_eq!(&serde_json::to_string(&new_ice_candidate_struct).unwrap(), ice_candidate_text);
+    assert_eq!(
+        &serde_json::to_string(&new_ice_candidate_struct).unwrap(),
+        ice_candidate_text
+    );
 }
 
 #[test]
@@ -88,5 +97,8 @@ fn test_serializing_assign_message() {
 
     let assign_message_text = r#"{"type":"assign","name":"4fe681ad-aba1-4732-89df-ee784b7d4abf"}"#;
 
-    assert_eq!(&serde_json::to_string(&assign_message_struct).unwrap(), assign_message_text);
+    assert_eq!(
+        &serde_json::to_string(&assign_message_struct).unwrap(),
+        assign_message_text
+    );
 }
