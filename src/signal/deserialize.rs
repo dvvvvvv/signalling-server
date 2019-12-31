6cdbbe2e900578811d2e945537b862b0cedc64d1
dvvvvvv <dvvvvvv@dvvvvvv.com>
2019-12-31 14:41:29 +0900

    add deserialize tests

--- src/signal/deserialize.rs
+++ src/signal/deserialize.rs
@@ -113,3 +113,68 @@ impl<'de> Visitor<'de> for AssignedNameVisitor {
         Err(M::Error::missing_field("name"))
     }
 }
+
+#[test]
+fn test_deserealizing_offer_signal() {
+    use super::SessionDescriptionMessage;
+
+    let offer_signal_text = r#"{"type":"offer","name":"3872379c-4743-4a7d-b2ee-79cf7368cf58","target":"4fe681ad-aba1-4732-89df-ee784b7d4abf","sdp":"sdp"}"#;
+
+    let offer_signal_struct = Signal::Offer(SessionDescriptionMessage {
+        target: "4fe681ad-aba1-4732-89df-ee784b7d4abf".to_owned(),
+        name: "3872379c-4743-4a7d-b2ee-79cf7368cf58".to_owned(),
+        sdp: "sdp".to_owned(),
+    });
+
+    assert_eq!(
+        serde_json::from_str::<Signal>(offer_signal_text).unwrap(),
+        offer_signal_struct
+    );
+}
+
+#[test]
+fn test_deserializing_answer_signal() {
+    use super::SessionDescriptionMessage;
+
+    let answer_signal_text = r#"{"type":"answer","name":"3872379c-4743-4a7d-b2ee-79cf7368cf58","target":"4fe681ad-aba1-4732-89df-ee784b7d4abf","sdp":"sdp"}"#;
+
+    let answer_signal_struct = Signal::Answer(SessionDescriptionMessage {
+        target: "4fe681ad-aba1-4732-89df-ee784b7d4abf".to_owned(),
+        name: "3872379c-4743-4a7d-b2ee-79cf7368cf58".to_owned(),
+        sdp: "sdp".to_owned(),
+    });
+
+    assert_eq!(
+        serde_json::from_str::<Signal>(&answer_signal_text).unwrap(),
+        answer_signal_struct
+    );
+}
+
+#[test]
+fn test_deserializing_new_ice_candidate_signal() {
+    use super::IceCandidate;
+
+    let new_ice_candidate_text = r#"{"type":"new_ice_candidate","target":"4fe681ad-aba1-4732-89df-ee784b7d4abf","candidate":"candidate"}"#;
+
+    let new_ice_candidate_struct = Signal::NewIceCandidate(IceCandidate {
+        target: "4fe681ad-aba1-4732-89df-ee784b7d4abf".to_owned(),
+        candidate: "candidate".to_owned(),
+    });
+
+    assert_eq!(
+        serde_json::from_str::<Signal>(&new_ice_candidate_text).unwrap(),
+        new_ice_candidate_struct
+    );
+}
+
+#[test]
+fn test_serializing_assign_message() {
+    let assign_message_text = r#"{"type":"assign","name":"4fe681ad-aba1-4732-89df-ee784b7d4abf"}"#;
+
+    let assign_message_struct = Signal::Assign("4fe681ad-aba1-4732-89df-ee784b7d4abf".to_owned());
+
+    assert_eq!(
+        serde_json::from_str::<Signal>(&assign_message_text).unwrap(),
+        assign_message_struct
+    );
+}
