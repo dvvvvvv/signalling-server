24ee770bb88affdf79b32d0ed0f6525120fed0e9
dvvvvvv <dvvvvvv@dvvvvvv.com>
2019-12-31 13:01:47 +0900

    add serializing unit tests

--- src/signal/serialize.rs
+++ src/signal/serialize.rs
@@ -37,3 +37,56 @@ impl Serialize for Signal {
         }
     }
 }
+
+#[test]
+fn test_serealizing_offer_signal() {
+    use super::SessionDescriptionMessage;
+
+    let offer_signal_struct = Signal::Offer(SessionDescriptionMessage {
+        target: "4fe681ad-aba1-4732-89df-ee784b7d4abf".to_owned(),
+        name: "3872379c-4743-4a7d-b2ee-79cf7368cf58".to_owned(),
+        sdp: "sdp".to_owned(),
+    });
+
+    let offer_signal_text = r#"{"type":"offer","name":"3872379c-4743-4a7d-b2ee-79cf7368cf58","target":"4fe681ad-aba1-4732-89df-ee784b7d4abf","sdp":"sdp"}"#;
+
+    assert_eq!(&serde_json::to_string(&offer_signal_struct).unwrap(), offer_signal_text);
+}
+
+#[test]
+fn test_serializing_answer_signal() {
+    use super::SessionDescriptionMessage;
+
+    let answer_signal_struct = Signal::Answer(SessionDescriptionMessage {
+        target: "4fe681ad-aba1-4732-89df-ee784b7d4abf".to_owned(),
+        name: "3872379c-4743-4a7d-b2ee-79cf7368cf58".to_owned(),
+        sdp: "sdp".to_owned(),
+    });
+
+    let answer_signal_text = r#"{"type":"answer","name":"3872379c-4743-4a7d-b2ee-79cf7368cf58","target":"4fe681ad-aba1-4732-89df-ee784b7d4abf","sdp":"sdp"}"#;
+
+    assert_eq!(&serde_json::to_string(&answer_signal_struct).unwrap(), answer_signal_text);
+}
+
+#[test]
+fn test_serializing_new_ice_candidate_signal() {
+    use super::IceCandidate;
+
+    let new_ice_candidate_struct = Signal::NewIceCandidate(IceCandidate{
+        target: "4fe681ad-aba1-4732-89df-ee784b7d4abf".to_owned(),
+        candidate: "candidate".to_owned(),
+    });
+
+    let ice_candidate_text = r#"{"type":"new_ice_candidate","target":"4fe681ad-aba1-4732-89df-ee784b7d4abf","candidate":"candidate"}"#;
+
+    assert_eq!(&serde_json::to_string(&new_ice_candidate_struct).unwrap(), ice_candidate_text);
+}
+
+#[test]
+fn test_serializing_assign_message() {
+    let assign_message_struct = Signal::Assign("4fe681ad-aba1-4732-89df-ee784b7d4abf".to_owned());
+
+    let assign_message_text = r#"{"type":"assign","name":"4fe681ad-aba1-4732-89df-ee784b7d4abf"}"#;
+
+    assert_eq!(&serde_json::to_string(&assign_message_struct).unwrap(), assign_message_text);
+}
